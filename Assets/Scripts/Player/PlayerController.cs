79f4d0b40fca50f3bda288e2517c681ad285c90b
ayse <aa14g23@soton.ac.uk>
2025-02-04 02:25:34 +0000

    added player dash + trail, updated movement
    i added the ability to dash with the spacebar key and made a trail that appears when this action occurs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,27 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Movement speed
+    public float moveSpeed;
+    public Rigidbody2D rb2d;
+    private Vector2 moveInput;
+    
 
-    private Rigidbody2D rb;
-    private Vector2 movement;
+    private float activeMoveSpeed;
+    public float dashSpeed;
 
+    public float dashLength = .5f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
+    [SerializeField] private TrailRenderer tr;
+
+    // Start is called before the first frame update
     void Start()
     {
-        // Get reference to the Rigidbody2D component
-        rb = GetComponent<Rigidbody2D>();
+        activeMoveSpeed = moveSpeed;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Get movement input from arrow keys or WASD
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-    }
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
 
-    void FixedUpdate()
-    {
-        // Move the player using Rigidbody2D
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        moveInput.Normalize();
+
+        rb2d.velocity = moveInput * activeMoveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashCoolCounter <=0 && dashCounter <=0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+                tr.emitting = true;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+                tr.emitting = false;
+            }
+        }
+
+        if(dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
     }
 }
