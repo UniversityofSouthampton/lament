81a5535c49cdf63419619f0e1f1b0500460e6af9
ayse <aa14g23@soton.ac.uk>
2025-04-19 22:03:09 +0100

    patch
    began work on the tutorial cutscene
    began developing the dialogue system
    added in reaction bubbles
    began on the timeline for animations within in the cutscene

---
+++ Assets/Scripts/Dialogue/DialogueBaseClass.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+namespace DialogueSystem
+{
+    public class DialogueBaseClass : MonoBehaviour
+    {
+        public bool finished {  get; private set; }
+        protected IEnumerator WriteText(string input, TMP_Text textHolder, float delay, AudioClip sound, float delayBetweenLines)
+        {
+            for(int i = 0; i <input.Length; i++)
+            {
+                textHolder.text += input[i];
+                SoundManager.instance.PlaySound(sound);
+                yield return new WaitForSeconds(delay);
+            }
+            yield return new WaitForSeconds(delayBetweenLines);
+            finished = true;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Dialogue/DialogueLine.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private TMP_Text textHolder;
+        [SerializeField] private string input;
+
+        [Header("Time parameters")]
+        [SerializeField] private float delay;
+        [SerializeField] private float delayBetweenLines;
+
+        [Header("Sound")]
+        [SerializeField] private AudioClip sound;
+
+        private bool continueToNextLine = false;
+        private void Awake()
+        {
+            textHolder = GetComponent<TMP_Text>();
+            textHolder.text = "";
+        }
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder, delay, sound, delayBetweenLines));
+        }
+        public void ContinueDialogue()
+        {
+            continueToNextLine = true;
+        }
+    }
+}
+
