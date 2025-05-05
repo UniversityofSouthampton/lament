111eac38eddb620e65ba333e0113ec74fe5c88ff
ejay <et4g23@soton.ac.uk>
2025-05-05 15:21:47 +0100

    FEAT
    The boss gate now:
    - Has a UI pop-up to let the player know whether they have the key or not
    - Takes the player back to the HUB if they do not have the key
    [Will take the player to the boss cutscene if they have the key, waiting for cutscene to be completed]

---
+++ Assets/Scripts/Enemies/BossGateway.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossGateway : MonoBehaviour
+{
+
+    public GameObject hasKey;
+    public GameObject hasNoKey;
+    private float UIDuration = 2.5f;
+
+    private bool playerInside = false;
+
+
+    void Update()
+    {
+
+        if(playerInside && Input.GetKeyDown(KeyCode.E))
+        {
+            if(PlayerStatsManager.Instance.hasBossKey == true)
+            {
+                StartCoroutine(hasKeyUI());
+            }
+
+            if(PlayerStatsManager.Instance.hasBossKey == false)
+            {
+                StartCoroutine(hasNoKeyUI());
+            }
+        }
+    }
+
+    private IEnumerator hasKeyUI()
+    {
+        hasKey.SetActive(true);
+        yield return new WaitForSeconds(UIDuration);
+        hasKey.SetActive(false);
+    }
+
+    private IEnumerator hasNoKeyUI()
+    {
+        hasNoKey.SetActive(true);
+        yield return new WaitForSeconds(UIDuration);
+        hasNoKey.SetActive(false);
+        yield return SceneManager.LoadSceneAsync("Hub");
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+}
